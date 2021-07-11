f9ef788149cb4469a8c6d0ebbaf7acbce616726a
Priyam <priyamroy29@gmail.com>
2021-07-11 15:57:47 +0530

    fixed typo in `webhook`

--- ditto/utils/webhooks.py
+++ ditto/utils/webhooks.py
@@ -10,9 +10,9 @@ __all__ = ("EmbedWebhookLogger",)
 class EmbedWebhookLogger:
     _to_log: list[discord.Embed]
 
-    def __init__(self, webhool_url: str, *, loop: asyncio.BaseEventLoop = None) -> None:
+    def __init__(self, webhook_url: str, *, loop: asyncio.BaseEventLoop = None) -> None:
         self.loop = loop or asyncio.get_event_loop()
-        self._webhook_url = webhool_url
+        self._webhook_url = webhook_url
         self._to_log = []
 
         self._session = aiohttp.ClientSession()
